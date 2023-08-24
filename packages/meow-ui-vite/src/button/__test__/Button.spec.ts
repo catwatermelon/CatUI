e1cc24d5bb1f1d8ee778e1c7e11fe693b99908cf
lzt <842835321@qq.com>
2023-08-24 19:10:50 +0800

    feat: 添加 codecov 覆盖率报告

--- packages/meow-ui-vite/src/button/__test__/Button.spec.ts
+++ packages/meow-ui-vite/src/button/__test__/Button.spec.ts
@@ -3,74 +3,91 @@ import Button from "../index.vue";
 import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
 // 测试分组
-describe('Button-Text', () => {
-    // mount
-    test("mount  @vue/test-utils", () => {
-        // @vue/test-utils
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            }
-        });
-
-        // 断言
-        expect(wrapper.text()).toBe("Button");
-    });
-})
-
-
-
-describe('Button-Bg-Color', () => {
-    test("primary", () => {
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            },
-            props: {
-                type: 'primary'
-            }
-        });
-        expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-400')).toBe(true)
+describe("Button-Text", () => {
+  // mount
+  test("mount  @vue/test-utils", () => {
+    // @vue/test-utils
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
     });
 
-    test("primary-ghost", () => {
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            },
-            props: {
-                type: 'primary',
-                ghost: true
-            }
-        });
-        expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-200')).toBe(true)
-    });
-})
+    // 断言
+    expect(wrapper.text()).toBe("Button");
+  });
+});
 
-
-describe('Button-Round', () => {
-    test("rounded-full", () => {
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            },
-            props: {
-                type: 'primary',
-                round: true
-            }
-        });
-        expect(wrapper.classes().map(v => v.replace('\n', '')).includes('rounded-full')).toBe(true)
+describe("Button-Bg-Color", () => {
+  test("primary", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        type: "primary",
+      },
     });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-400"),
+    ).toBe(true);
+  });
 
-    test("rounded-lg", () => {
-        const wrapper = shallowMount(Button, {
-            slots: {
-                default: 'Button'
-            },
-            props: {
-                type: 'primary',
-            }
-        });
-        expect(wrapper.classes().map(v => v.replace('\n', '')).includes('rounded-lg')).toBe(true)
+  test("primary-ghost", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        type: "primary",
+        ghost: true,
+      },
     });
-})
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-200"),
+    ).toBe(true);
+  });
+});
+
+describe("Button-Round", () => {
+  test("rounded-full", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        type: "primary",
+        round: true,
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("rounded-full"),
+    ).toBe(true);
+  });
+
+  test("rounded-lg", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        type: "primary",
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("rounded-lg"),
+    ).toBe(true);
+  });
+});
