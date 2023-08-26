389ee9acbae82b5f58b26c296bd83fb53f720e42
lzt <842835321@qq.com>
2023-08-27 03:43:21 +0800

    chore: 修改 package.json 中 main 字段

--- packages/meow-ui-docs/docs/.vitepress/theme/index.ts
+++ packages/meow-ui-docs/docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
 import { useComponents } from './useComponents'
 import MeowUI from 'meow-ui-vite';
-import 'meow-ui-vite/dist/assets/entry.css';
+import 'meow-ui-vite/style';
 import './style/var.css'
 
 export default {
